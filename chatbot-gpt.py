274252e21091410e9e4afb862aa3b060346d933a
assiri <assiri@hotmail.com>
2023-09-04 05:40:11 +0300

    Update chatbot-gpt.py

--- chatbot-gpt.py
+++ chatbot-gpt.py
@@ -10,19 +10,23 @@ messages = [
     {"role": "system", "content": "You are a helpful and kind AI Assistant."},
 ]
 
+
 def chatbot(input):
     if input:
         messages.append({"role": "user", "content": input})
-        chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
-        )
+        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
         reply = chat.choices[0].message.content
         messages.append({"role": "assistant", "content": reply})
         return reply
 
+
 inputs = gr.inputs.Textbox(lines=7, label="Chat with AI")
 outputs = gr.outputs.Textbox(label="Reply")
 
-gr.Interface(fn=chatbot, inputs=inputs, outputs=outputs, title="ChatGPT : Meow!",
-             description="Ask anything you want",
-             theme="compact").launch(share=True)
+gr.Interface(
+    fn=chatbot,
+    inputs=inputs,
+    outputs=outputs,
+    title="ChatGPT : Test!",
+    description="Ask anything you want",
+).launch()
